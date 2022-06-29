63190834b6d7c6b033f7b66b8ae836b8153a625e
Detsibli <jaimecabriales1@gmail.com>
2022-06-28 23:58:05 -0500

    Updated Player.gd
    Added comments

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ func _ready():
 func _physics_process(delta):
 		var motion = Vector2()
 		
+		#Simple Movement
 		if Input.is_action_pressed("up"):
 			motion.y -= 1
 		if Input.is_action_pressed("down"):
@@ -18,13 +19,17 @@ func _physics_process(delta):
 		if Input.is_action_pressed("left"):
 			motion.x -= 1
 		
+		# If Space has just been pressed, speed is multiplied by 2
 		if Input.is_action_just_pressed("space"):
 			moveSpeed = moveSpeed * 2
 		
+		# When Space is released, speed goes back to the default value
 		if Input.is_action_just_released("space"):
 			moveSpeed = 250
 		
+		# Normalizes motion and uses move_and_slide to move Player
 		motion - motion.normalized()
 		motion = move_and_slide(motion * moveSpeed)
 		
+		# Rotates Player sprite to the mouses current position
 		look_at(get_global_mouse_position())
