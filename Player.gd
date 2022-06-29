39ce2cb761432955cbf08ab38dd6ef50c47a8a6e
Detsibli <jaimecabriales1@gmail.com>
2022-06-29 17:22:10 -0500

    Added bullet function

--- Player.gd
+++ Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 var moveSpeed = 250
+var bulletSpeed = 2000
+var bullet = preload("res://Bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -33,3 +35,18 @@ func _physics_process(delta):
 		
 		# Rotates Player sprite to the mouses current position
 		look_at(get_global_mouse_position())
+
+
+		# When LMB is pressed, fire a bullet
+		if Input.is_action_just_pressed("LMB"):
+			fire()
+			
+		
+func fire():
+	var bullet_inst = bullet.instance()
+	bullet_inst.position = get_global_position()
+	bullet_inst.rotation_degrees = rotation_degrees
+	bullet_inst.apply_impulse(Vector2(),Vector2(bulletSpeed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_inst)
+	
+	
